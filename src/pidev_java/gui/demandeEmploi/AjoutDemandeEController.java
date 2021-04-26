c2cce3ab5f5fe8529de6e4269e14499e502db605
ELYES <elyes.kabous@esprit.tn>
2021-04-26 08:55:06 +0100

    crud v2

--- src/pidev_java/gui/demandeEmploi/AjoutDemandeEController.java
+++ src/pidev_java/gui/demandeEmploi/AjoutDemandeEController.java
@@ -13,9 +13,12 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javax.swing.JOptionPane;
 import pidev_java.entities.DemandeEmploi;
 import pidev_java.entities.Freelancer;
@@ -42,38 +45,84 @@ public class AjoutDemandeEController implements Initializable {
     private Button btnEnvoyerDemande;
     @FXML
     private Button btnFile;
+     @FXML
+    private VBox Vboxadd ;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+      
     }    
 
     
     @FXML
     private void AjoutDemandeE(ActionEvent event)throws IOException  {
-        
+          Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Form non valide !");
+            alert.setContentText("verifie les champs !");
+        Label labelresponse= new Label();
         Freelancer f = Freelancer.getInstance();
         DemandeEmploiService sd = new DemandeEmploiService();
         DemandeEmploi d = new DemandeEmploi();
         d.setFreelancer_id(f.getId());
         d.setDate_creation(Date.valueOf(LocalDate.MAX));
-        d.setDescription(tfDescription.getText());
-        d.setDiplome(tfDiplome.getText());
-        d.setDomaine(tfDomaine.getText());
-        d.setLettre(tflettre.getText());
-        String s = tfSalaire.getText();
-        d.setSalaire(Float.parseFloat(s));
+        Boolean valid =true;
+        if(!(tfDescription.getText().isEmpty()) ||!(tfDescription.getText().length()>=30)){
+            d.setDescription(tfDescription.getText());
+        }else{
+            valid=false;
+            labelresponse.setText("la description entrée est non valide");
+            Vboxadd.getChildren().add(labelresponse);
+        }
+        if(!(tfDiplome.getText().isEmpty())){
+           d.setDiplome(tfDiplome.getText()); 
+        }else
+        {
+            valid=false;
+        }
+        if(!tfDomaine.getText().isEmpty()){
+            d.setDomaine(tfDomaine.getText());
+        }else
+        {
+            valid=false;
+        }
+        if(!tflettre.getText().isEmpty()){
+             d.setLettre(tflettre.getText());
+        }else{
+            valid=false;
+        }
+            
+       if(!tfSalaire.getText().isEmpty()){
+            String s = tfSalaire.getText();
+            try {
+              
+                d.setSalaire(Float.parseFloat(s));
+           } catch (NumberFormatException  e) {
+               valid=false;
+           }
        
-        sd.ajouter(d);
-        tfDescription.setText("");
+       }else
+       {
+           valid=false;
+       }
+       
+       if(valid==true){
+          sd.ajouter(d); 
+           tfDescription.setText("");
         tfDiplome.setText("");
         tfDomaine.setText("");
         tflettre.setText("");
         tfSalaire.setText("");
         JOptionPane.showMessageDialog(null, "Demande Envoyer !");
+       }else
+       {
+           alert.showAndWait();
+       }
+        
+       
         
     }
     
